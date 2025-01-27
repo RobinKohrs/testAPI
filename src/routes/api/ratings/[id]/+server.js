d3b767ecfb6c7827a0917269197d47e240227c3d
RobinKohrs <robin.kohrs@gmx.de>
2025-01-27 16:57:45 +0100

    ..

--- src/routes/api/ratings/[id]/+server.js
+++ src/routes/api/ratings/[id]/+server.js
@@ -18,7 +18,7 @@ export async function GET({ params }) {
     // Check if the response is OK
     if (!response.ok) {
       throw new Error(
-        `HTTP error! status: ${response.status} ${response.statusText} ${response} ${response.body}`
+        `HTTP error! status: ${response.status} ${response.statusText} ${response} ${response.body} ${response.headers} ${response.type}`
       );
     }
 
